4a0211be840071aa245810857ff41fcfb2e798fb
Adam Yu <adamxhy123@gmail.com>
2019-01-26 18:27:26 -0500

    fix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-import 'choice.dart';
+import 'splash.dart';
 import 'model.dart';
 import 'login.dart';
 import 'robin.dart';
