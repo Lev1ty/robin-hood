1aeb181b41aa68fca4fcc6b175d757f17910e81b
Liam Ward <liam.ward144@gmail.com>
2019-01-26 18:53:55 -0500

    Donor Stats Scaffolding

--- lib/robin.dart
+++ lib/robin.dart
@@ -29,12 +29,14 @@ class _RobinState extends State<Robin> with SingleTickerProviderStateMixin {
         title: Text("User Name"),
       ),
       body: Center(
-          child: Text("User data will go here: ")
+        child: ListView(
+          children: list
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              icon: Icon(Icons.show_chart, color: Colors.blue[500]), title: Text('Chart')),
+              icon: Icon(Icons.pie_chart_outlined, color: Colors.blue[500]), title: Text('Chart')),
           BottomNavigationBarItem(
               icon: Icon(Icons.attach_money, color: Colors.transparent), title: Text(" ")),
           BottomNavigationBarItem(icon: Icon(Icons.map, color: Colors.blue[500]), title: Text("Distribution"))
@@ -52,5 +54,25 @@ class _RobinState extends State<Robin> with SingleTickerProviderStateMixin {
 
     );
   }
+  List<Widget> list = <Widget>[
+    ListTile(
+      title: Text("Data Set 1"),
+      leading: Icon(
+        Icons.group),
+    ),
+    ListTile(
+        title: Text("Data Set 2"),
+        leading: Icon(
+          Icons.place),
+  ),
+    ListTile(
+        title: Text("Data Set 3"),
+        leading: Icon(
+          Icons.history),
+  ),
+    ListTile(
+        title: Text("Data Set 4"),
+        leading: Icon(
+          Icons.show_chart),
+  );
 
-}
